3e8dafdaa8b40ef6e61add21e30c9fd175dc250d
Denus bc2025-6 <denisilcuk@gmail.com>
2025-11-30 16:15:24 +0200

    Переробив головний головний файл

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-// ---------- 1. Налаштування аргументів командного рядка ----------
 program
   .requiredOption("-h, --host <string>", "Адреса сервера (обовʼязково)")
   .requiredOption("-p, --port <number>", "Порт сервера (обовʼязково)")
@@ -15,7 +14,6 @@ program
 program.parse(process.argv);
 const options = program.opts();
 
-// ---------- 2. Створення директорії cache якщо її не існує ----------
 const cacheDir = path.resolve(options.cache);
 
 if (!fs.existsSync(cacheDir)) {
@@ -23,7 +21,6 @@ if (!fs.existsSync(cacheDir)) {
   fs.mkdirSync(cacheDir, { recursive: true });
 }
 
-// ---------- 3. Запуск HTTP сервера ----------
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Server is running!\n");
